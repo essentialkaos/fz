045e47c0e38890ef2dd4e7ea7cc9e781d5c9cd86
Anton Novojilov <andy@essentialkaos.com>
2019-03-02 00:48:26 +0300

    Improvements

--- fz.go
+++ fz.go
@@ -10,13 +10,17 @@ package main
 import (
 	"bufio"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"pkg.re/essentialkaos/ek.v10/fmtc"
 	"pkg.re/essentialkaos/ek.v10/fmtutil"
 	"pkg.re/essentialkaos/ek.v10/options"
+	"pkg.re/essentialkaos/ek.v10/signal"
 	"pkg.re/essentialkaos/ek.v10/strutil"
+	"pkg.re/essentialkaos/ek.v10/timeutil"
 	"pkg.re/essentialkaos/ek.v10/usage"
 	"pkg.re/essentialkaos/ek.v10/usage/update"
 )
@@ -43,13 +47,13 @@ type Info struct {
 	DateTime    string
 	Workers     int
 	Corpus      int
-	CorpusDate  string
+	CorpusDur   int64
 	Crashers    int
 	Restarts    string
 	Execs       int
 	ExecsPerSec int
 	Cover       int
-	Uptime      string
+	Uptime      int64
 }
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -63,8 +67,15 @@ var optMap = options.Map{
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
+var startInfo Info
+var prevInfo Info
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
 // main is main func
 func main() {
+	runtime.GOMAXPROCS(3)
+
 	_, errs := options.Parse(optMap)
 
 	if len(errs) != 0 {
@@ -91,6 +102,12 @@ func main() {
 		os.Exit(0)
 	}
 
+	signal.Handlers{
+		signal.INT:  signalHandler,
+		signal.TERM: signalHandler,
+		signal.QUIT: signalHandler,
+	}.TrackAsync()
+
 	processInput()
 }
 
@@ -99,71 +116,95 @@ func processInput() {
 	r := bufio.NewReader(os.Stdin)
 	s := bufio.NewScanner(r)
 
-	var prevInfo Info
-
 	fmtc.TPrintf("{s-}Starting tests…{!}")
 
 	for s.Scan() {
 		data := s.Text()
 
 		if isShutdownMessage(data) {
-			fmtc.Println("")
-			os.Exit(0)
+			time.Sleep(time.Minute)
 		}
 
-		info := parseInfoLine(data)
+		info, ok := parseInfoLine(data)
 
-		renderInfo(info, prevInfo)
+		if !ok {
+			fmtc.TPrintf("")
+			printError(data)
+			os.Exit(1)
+		}
+
+		renderInfo(info)
 
 		prevInfo = info
+
+		if startInfo.Uptime == 0 && info.Cover != 0 {
+			startInfo = info
+		}
 	}
 }
 
 // parseInfoLine parses line with go-fuzz output data
-func parseInfoLine(data string) Info {
+func parseInfoLine(data string) (Info, bool) {
 	info := Info{}
 
 	dataSlice := strings.Split(data, ",")
 
 	if len(dataSlice) < 7 {
-		return info
+		return info, false
 	}
+
+	corpusDate := strings.Trim(strutil.ReadField(dataSlice[1], 2, false, " "), "(")
+	uptime := strutil.ReadField(dataSlice[6], 1, false, " ")
 
 	info.DateTime = strutil.ReadField(dataSlice[0], 0, false, " ")
 	info.DateTime += " " + strutil.ReadField(dataSlice[0], 1, false, " ")
 	info.Workers, _ = strconv.Atoi(strutil.ReadField(dataSlice[0], 3, false, " "))
 	info.Corpus, _ = strconv.Atoi(strutil.ReadField(dataSlice[1], 1, false, " "))
-	info.CorpusDate = strutil.ReadField(dataSlice[1], 2, false, " ") + ")"
+	info.CorpusDur = timeutil.ParseDuration(corpusDate)
 	info.Crashers, _ = strconv.Atoi(strutil.ReadField(dataSlice[2], 1, false, " "))
 	info.Restarts = strutil.ReadField(dataSlice[3], 1, false, " ")
 	info.Execs, _ = strconv.Atoi(strutil.ReadField(dataSlice[4], 1, false, " "))
 	execsPerSec := strings.Trim(strutil.ReadField(dataSlice[4], 2, false, " "), "(/sec)")
 	info.ExecsPerSec, _ = strconv.Atoi(execsPerSec)
 	info.Cover, _ = strconv.Atoi(strutil.ReadField(dataSlice[5], 1, false, " "))
-	info.Uptime = strutil.ReadField(dataSlice[6], 1, false, " ")
+	info.Uptime = timeutil.ParseDuration(uptime)
 
-	return info
+	return info, true
 }
 
 // renderInfo render line with info
-func renderInfo(cur Info, prev Info) {
+func renderInfo(cur Info) {
 	var crashersTag string
 
-	workersTag := getIndicatorTag(cur.Workers, prev.Workers)
-	corpusTag := getIndicatorTag(cur.Corpus, prev.Corpus)
-	coverTag := getIndicatorTag(cur.Cover, prev.Cover)
+	workersTag := getIndicatorTag(cur.Workers, prevInfo.Workers)
+	corpusTag := getIndicatorTag(cur.Corpus, prevInfo.Corpus)
+	coverTag := getIndicatorTag(cur.Cover, prevInfo.Cover)
 
 	if cur.Crashers != 0 {
 		crashersTag = "{r}"
 	}
 
-	execsArrow := getDynamicsArrow(cur.ExecsPerSec, prev.ExecsPerSec)
+	execsArrow := getDynamicsArrow(cur.ExecsPerSec, prevInfo.ExecsPerSec)
 
 	fmtc.TPrintf(
-		"{s}%s{!} {s-}[%s]{!} {*}Workers:{!} "+workersTag+"%d{!} {s}│{!} {*}Corpus:{!} "+corpusTag+"%s{!} {s-}%s{!} {s}│{!} {*}Crashers:{!} "+crashersTag+"%d {s}│{!} {*}Restarts:{!} %s {s}│{!} {*}Cover:{!} "+coverTag+"%s{!} {s}│{!} {*}Execs:{!} {s}%s{!}%s{s}/s{!} {s-}(%s){!}",
-		cur.DateTime, cur.Uptime, cur.Workers, fmtutil.PrettyNum(cur.Corpus),
-		cur.CorpusDate, cur.Crashers, cur.Restarts, fmtutil.PrettyNum(cur.Cover),
+		"{s}%s{!} {s-}[%s]{!} {*}Workers:{!} "+workersTag+"%d{!} {s}│{!} {*}Corpus:{!} "+corpusTag+"%s{!} {s-}(%s){!} {s}│{!} {*}Crashers:{!} "+crashersTag+"%d {s}│{!} {*}Restarts:{!} %s {s}│{!} {*}Cover:{!} "+coverTag+"%s{!} {s}│{!} {*}Execs:{!} {s}%s{!}%s{s}/s{!} {s-}(%s){!}",
+		cur.DateTime, formatDuration(cur.Uptime), cur.Workers, fmtutil.PrettyNum(cur.Corpus),
+		formatDuration(cur.CorpusDur), cur.Crashers, cur.Restarts, fmtutil.PrettyNum(cur.Cover),
 		execsArrow, fmtutil.PrettyNum(cur.ExecsPerSec), fmtutil.PrettyNum(cur.Execs),
+	)
+}
+
+// printResults prints tests results
+func printResults() {
+	corpus := formatResultNum(prevInfo.Corpus - startInfo.Corpus)
+	crashers := formatResultNum(prevInfo.Crashers - startInfo.Crashers)
+	cover := formatResultNum(prevInfo.Cover - startInfo.Cover)
+	duration := timeutil.PrettyDuration(prevInfo.Uptime)
+	execs := fmtutil.PrettyNum(prevInfo.Execs)
+
+	fmtc.TPrintf(
+		"{*}Duration:{!} %s {s}│{!} {*}Execs:{!} %s {s}│{!} {*}Corpus:{!} %s {s}│{!} {*}Crashers:{!} %s {s}│{!} {*}Cover:{!} %s\n",
+		duration, execs, corpus, crashers, cover,
 	)
 }
 
@@ -191,6 +232,31 @@ func getDynamicsArrow(v1, v2 int) string {
 	default:
 		return ""
 	}
+}
+
+// formatDuration formats duration
+func formatDuration(d int64) string {
+	var min, sec int64
+
+	min = d / 60
+	sec = d % 60
+
+	return fmtc.Sprintf("%01d:%02d", min, sec)
+}
+
+// formatResultNum formats number for results
+func formatResultNum(v int) string {
+	if v <= 0 {
+		return "0"
+	}
+
+	return fmtc.Sprintf("+%d", v)
+}
+
+// signalHandler is signal handler
+func signalHandler() {
+	printResults()
+	os.Exit(0)
 }
 
 // hasStdinData return true if there is some data in stdin
